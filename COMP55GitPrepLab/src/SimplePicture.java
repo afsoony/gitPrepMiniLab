7c34cf85b92ea45d34fc0277b095a262e9f00e76
afsoony <67535264+afsoony@users.noreply.github.com>
2024-09-25 09:18:06 -0700

    Initial commit

---
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -0,0 +1,23 @@
+//comment one
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class SimplePicture extends GraphicsProgram {
+	public static int SIZE = 700;
+	
+	public void init() {
+		setSize(SIZE, SIZE);
+	}
+	
+	public void run() {
+		GImage robot = new GImage("robot.jpg", 200, 100);
+		add(robot);
+		GLabel label = new GLabel("How I feel when using github", 200, 400);
+		add(label);
+	}
+	
+	public static void main(String[] args) {
+		new SimplePicture().start();
+	}
+}
